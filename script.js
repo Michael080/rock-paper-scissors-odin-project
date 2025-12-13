96bd7e51f1dd8bbd5c41c59d2643391eaccf6a2d
Michael080 <mrollin808@gmail.com>
2025-12-13 15:35:20 -0700

    Complete steps 4 - 5

--- script.js
+++ script.js
@@ -18,15 +18,42 @@ function getComputerChoice() {
 
 // prompt user for weapon selection
 function getHumanChoice() {
-  let weapon = prompt('Select your weapon');
+  // TODO: remove default 'spock' once logic is complete
+  let weapon = prompt('Select your weapon', 'spock');
+  weapon = weapon.toLowerCase();
   return weapon;
 }
 
 function Player(name) {
   this.name = name;
   // TODO: uncomment below line - temp. for easy iteration
-  //this.weapon = getHumanChoice()
-  this.weapon = 'scissors';
+  this.choice = getHumanChoice();
+  this.score = 0;
 }
 
-let player1 = new Player('player1');
+let computer = {
+  choice: getComputerChoice(),
+  score: 0,
+}
+
+computer.choice = getComputerChoice();
+
+let human = new Player('human');
+
+// WARNING: temporary values REMOVE AFTER TESTING SUCCEEDS
+//human.choice = 'spock';
+//computer.choice = 'lizard';
+
+function playRound(computerChoice, humanChoice) {
+  if (winConditions[humanChoice].includes(computerChoice)) {
+    // human score condition
+    human.score++;
+  } else if (winConditions[computerChoice].includes(humanChoice)) {
+    //computer win condition
+    computer.score++;
+  } else {
+    // draw condition
+  }
+}
+
+playRound(computer.choice, human.choice);
