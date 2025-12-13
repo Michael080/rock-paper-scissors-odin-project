c0ce63270e5bcf5822876ae761bca1ffe8d53549
Michael080 <mrollin808@gmail.com>
2025-12-13 14:45:49 -0700

    Complete step 2 write function getComputerChoice
    Odin Project Foundations - Unit "JS Basics Project: Rock Paper Scissors"

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const weapons = ['scissors', 'paper', 'rock', 'lizard', 'spock'];
+
+const winConditions = {
+  'scissors': ['paper', 'lizard'],
+  'paper': ['rock', 'spock'],
+  'rock': ['lizard', 'scissors'],
+  'lizard': ['spock', 'paper'],
+  'spock': ['scissors', 'rock']
+}
+
+function getRandom() {
+  return Math.floor(Math.random() * 5);
+}
+
+function getComputerChoice() {
+  return weapons[getRandom()];
+}
+
 // prompt user for weapon selection
 function getHumanChoice() {
   let weapon = prompt('Select your weapon');
@@ -6,7 +24,9 @@ function getHumanChoice() {
 
 function Player(name) {
   this.name = name;
-  this.weapon = getHumanChoice()
+  // TODO: uncomment below line - temp. for easy iteration
+  //this.weapon = getHumanChoice()
+  this.weapon = 'scissors';
 }
 
 let player1 = new Player('player1');
